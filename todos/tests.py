5f293d69696cbdbe8c182bbc2e7159ce791393b7
Ubuntu <ubuntu@ip-172-31-42-207.ap-southeast-2.compute.internal>
2025-05-06 20:13:51 +0000

    test change

--- todos/tests.py
+++ todos/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from .models import Todo
+<<<<<<< HEAD
 
 class TodoModelTest(TestCase):
     def test_str_representation(self):
@@ -27,3 +28,33 @@ class TodoViewTest(TestCase):
     def test_todo_in_context(self):
         response = self.client.get(reverse('todos:index'))
         self.assertIn(self.todo, response.context['todo_list'])  # Adjust context variable as needed
+=======
+
+class TodoModelTest(TestCase):
+    def test_str_representation(self):
+        todo = Todo(title="Sample Task")
+        self.assertEqual(str(todo), "Sample Task")
+
+    def test_default_completion(self):
+        todo = Todo(title="Task")
+        self.assertFalse(todo.isCompleted)
+
+class TodoViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.todo = Todo.objects.create(title="Initial Task")
+
+    def test_index_view_status_code(self):
+        response = self.client.get(reverse('todos:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_view_uses_correct_template(self):
+        response = self.client.get(reverse('todos:index'))
+        self.assertTemplateUsed(response, 'todos/index.html')
+
+    def test_todo_in_context(self):
+        response = self.client.get(reverse('todos:index'))
+        self.assertIn(self.todo, response.context['todo_list'])
+
+
+>>>>>>> 4befbd1 (test change)
