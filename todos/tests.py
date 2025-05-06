aae57b77ae4b31496d361476d214300fde334a71
Khunaal-Aryan <98578740+Khunaal-Aryan@users.noreply.github.com>
2025-05-07 01:04:50 +0530

    Update tests.py

--- todos/tests.py
+++ todos/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Todo
 
-# Create your tests here.
+class TodoModelTest(TestCase):
+    def test_str_representation(self):
+        todo = Todo(title="Sample Task")
+        self.assertEqual(str(todo), "Sample Task")
+
+    def test_default_completion(self):
+        todo = Todo(title="Task")
+        self.assertFalse(todo.completed)
+
+class TodoViewTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.todo = Todo.objects.create(title="Initial Task")
+
+    def test_index_view_status_code(self):
+        response = self.client.get(reverse('todos:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_view_uses_correct_template(self):
+        response = self.client.get(reverse('todos:index'))
+        self.assertTemplateUsed(response, 'todos/index.html')
+
+    def test_todo_in_context(self):
+        response = self.client.get(reverse('todos:index'))
+        self.assertIn(self.todo, response.context['todo_list'])  # Adjust context variable as needed
